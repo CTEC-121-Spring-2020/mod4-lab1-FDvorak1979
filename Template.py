6daad2d1142436576407971a6fb1cb59f04920e6
FDvorak1979 <f.dvorak2@students.clark.edu>
2020-05-11 12:16:56 -0700

    end of day

--- Template.py
+++ Template.py
@@ -13,9 +13,28 @@ Output: return value and description
 from graphics import *
 
 def main():
+'''
     win = GraphWin("title", 600, 300)
 
+    p = Point(100,200)
+    x = p.getX()
+    y = p.getY()
+    print("x and y:", x, y)
+
     input()
-    
+'''
+
+    win = GraphWin("Bill", 600, 600)
+
+    p1 = Point(50,50)
+    p2 = Point(100,100)
+    r1 = Rectangle(p1, p2)
+    r1.draw(win)
+
+    r2 = Rectangle(Point(200,200), Point(250,250))
+    r2.draw(win)
+
+    input()
+
 
 main()    
